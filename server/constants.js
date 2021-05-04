5ecc47f799aa375dcf8fac63fb024a18dd07282f
Vinícius Guedes <vynyguedes@gmail.com>
2021-05-04 01:32:43 -0300

    feat: endpoints de agendamento

--- server/constants.js
+++ server/constants.js
@@ -1,6 +1,105 @@
 var examples = {
     AGENDAMENTO_LIST: [
-    ],
+        {
+          "name": "Felipe Lima",
+          "cpf": "12019146589",
+          "dentist": 2,
+          "color": "deep-purple",
+          "start": new Date("Mon May 10 2021 09:00:00 GMT-0300"),
+          "end": new Date("Mon May 10 2021 09:30:00 GMT-0300"),
+          "timed": true
+        },
+        {
+          "name": "Carla Alves",
+          "cpf": "14725836910",
+          "dentist": 3,
+          "color": "cyan",
+          "start": new Date("Sat May 29 2021 14:00:00 GMT-0300"),
+          "end": new Date("Sat May 29 2021 14:30:00 GMT-0300"),
+          "timed": true
+        },
+        {
+          "name": "Igor Henrique",
+          "cpf": "96385274154",
+          "dentist": 1,
+          "color": "indigo",
+          "start": new Date("Tue Jun 15 2021 11:00:00 GMT-0300"),
+          "end": new Date("Tue Jun 15 2021 11:30:00 GMT-0300"),
+          "timed": true
+        },
+        {
+          "name": "Vinícius Galindo",
+          "cpf": "78945612330",
+          "dentist": 5,
+          "color": "orange",
+          "start": new Date("Wed May 05 2021 13:00:00 GMT-0300"),
+          "end": new Date("Wed May 05 2021 13:30:00 GMT-0300"),
+          "timed": true
+        },
+        {
+          "name": "Gabriel Luz",
+          "cpf": "98765432155",
+          "dentist": 0,
+          "color": "blue",
+          "start": new Date("Thu May 20 2021 10:00:00 GMT-0300"),
+          "end": new Date("Thu May 20 2021 10:30:00 GMT-0300"),
+          "timed": true
+        },
+        {
+          "name": "Deryk Alves",
+          "cpf": "75315985245",
+          "dentist": 6,
+          "color": "grey darken-1",
+          "start": new Date("Mon May 24 2021 11:00:00 GMT-0300"),
+          "end": new Date("Mon May 24 2021 11:30:00 GMT-0300"),
+          "timed": true
+        },
+        {
+          "name": "Heron",
+          "cpf": "78945612355",
+          "dentist": 3,
+          "color": "cyan",
+          "start": new Date("Mon May 24 2021 15:00:00 GMT-0300"),
+          "end": new Date("Mon May 24 2021 15:30:00 GMT-0300"),
+          "timed": true
+        },
+        {
+          "name": "Lucas Roberto",
+          "cpf": "45786314426",
+          "dentist": 4,
+          "color": "green",
+          "start": new Date("Fri May 14 2021 10:00:00 GMT-0300"),
+          "end": new Date("Fri May 14 2021 10:30:00 GMT-0300"),
+          "timed": true
+        },
+        {
+          "name": "Rogéria Lopes",
+          "cpf": "78545622455",
+          "dentist": 4,
+          "color": "green",
+          "start": new Date("Thu Jun 03 2021 16:00:00 GMT-0300"),
+          "end": new Date("Thu Jun 03 2021 16:30:00 GMT-0300"),
+          "timed": true
+        },
+        {
+          "name": "Joana Silva",
+          "cpf": "45214596325",
+          "dentist": 2,
+          "color": "deep-purple",
+          "start": new Date("Fri Jun 25 2021 17:00:00 GMT-0300"),
+          "end": new Date("Fri Jun 25 2021 17:30:00 GMT-0300"),
+          "timed": true
+        },
+        {
+          "name": "João Pedro Silva",
+          "cpf": "78547854785",
+          "dentist": 3,
+          "color": "cyan",
+          "start": new Date("Sun May 02 2021 10:00:00 GMT-0300"),
+          "end": new Date("Sun May 02 2021 10:30:00 GMT-0300"),
+          "timed": true
+        }
+      ],
     PRONTUARIO_LIST: [
         {
             id: '1',
@@ -83,4 +182,9 @@ function pushAtendimento (atendimento) {
     return examples.PRONTUARIO_LIST[index].atendimento
 }
 
-module.exports = { examples, pushProntuario, pushAtendimento }
+function pushAgendamento (agendamento) {
+    examples.AGENDAMENTO_LIST.push(agendamento)
+    return examples.AGENDAMENTO_LIST
+}
+
+module.exports = { examples, pushProntuario, pushAtendimento, pushAgendamento }
